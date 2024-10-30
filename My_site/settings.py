aa75d9eb7ce9e8b782ec1aa2268d5b43d8609937
Badr Discipline <badrdiscipline@Badrs-MacBook-Pro.local>
2024-10-30 20:52:19 +0100

    change

--- My_site/settings.py
+++ My_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@c+7p=sm=*kv1j$iy_h51gt+@7ml)^nt)akvek#h#se+1-hd_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
